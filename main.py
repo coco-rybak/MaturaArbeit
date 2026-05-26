70dcb7f647d0d4113ec0dc46c0a47003c151bc4b
Cora Rybak <cora.rybak@gmail.com>
2026-05-26 20:22:17 +0200

    Started project, happy now Pavel?

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
+from dataclasses import dataclass
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+@dataclass(repr=False)
+class Wall:
+    length: int
+    angle: int
+
+    def __repr__(self) -> str:
+        return f"This Wall is {self.length} m long and has a {self.angle} inclination."
+
+straight_wall = Wall(length=13, angle=0)
+five_wall= Wall(length=13, angle=5)
+ten_wall = Wall(length=13, angle=10)
+fifteen_wall = Wall(length=13, angle=15)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+@dataclass(repr=False)
+class Hold:
+    size: str
+    type: str
+    difficulty: int
+
+    def __repr__(self) -> str:
+     return f"This piece is a {self.size} {self.type} and has a difficulty rating of {self.difficulty}."
+
+piece1 = Hold(size="large", type="foothold", difficulty=1)
+piece2 = Hold(size= "medium", type= "jug", difficulty= 10)
+piece3 = Hold(size="small", type= "crimp", difficulty= 15)
+
+#@dataclass(repr=False)
+#class Route:
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+        print(piece1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Bewertung für Schwierigkeiten:
+# 3-4c: 0 - 0.2
+# 5a-5c : 0.2 - 0.3
+# 6a-6a+ : 0.3 - 0.4
+# 6b-6b+ : 0.4 - 0.5
+# 6c-6c+ : 0.5 - 0.6
+# 7a-7a+ : 0.6 - 0.7
+# 7b-7b+ : 0.7 - 0.8
+# 7c-7c+ : 0.8 - 0.9
+# 8a-9a : 0.9 - 1
+
+#Beispiel: 6b-6b+ Route, mit grossen Griffen, Wand bei 90°:
+#Wand: 0
+#Griffe: 0.1
+#Abstand: 0.25
+#Neigung: 0.1
+
+
+
+
+
+
